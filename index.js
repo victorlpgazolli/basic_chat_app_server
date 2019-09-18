0fd4e3d793b3e54217a4513a2f1a5cb88a346b0a
Victor Gazolli <victorgazolli@compmac.local>
2019-09-18 20:28:34 -0300

    heroku socket changes

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+// var express = require('express'),
+//     app = express(),
+//     server = require('http').createServer(app),
+//     io = require('socket.io').listen(server);
 
 
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on("chat message", msg => {
-        console.log(msg);
-        io.emit("chat message", msg);
-      });
+// io.on('connection', function (socket) {
+//     console.log('a user connected');
+//     socket.on("chat message", msg => {
+//         console.log(msg);
+//         io.emit("chat message", msg);
+//       });
+// });
+
+// server.listen(8080, () => {
+//     console.log('listening on '+ 8080);
+// });
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on("chat message", msg => {
+             console.log(msg);
+             io.emit("chat message", msg);
+           });
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-server.listen(8080, () => {
-    console.log('listening on '+ 8080);
-});
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
