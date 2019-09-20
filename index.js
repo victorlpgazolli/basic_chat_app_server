4fbb73a9f788ac72a09ebdd18d21ce416af03966
Victor Gazolli <victorgazolli@compmac.local>
2019-09-20 16:06:26 -0300

    telegram integration and heroku fix

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+process.env.NTBA_FIX_319 = 1;
+
 const express = require('express');
-const app = express();
-const server = require('http').createServer(express);
-const io = require('socket.io').listen(server);
-const port = 3000;
+const socketIO = require('socket.io');
+const path = require('path');
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on("chat message", msg => {
-        console.log(msg);
-        io.emit("chat message", msg);
-      });
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+var TelegramBot = require("node-telegram-bot-api");
+var token = "843163756:AAEDLcPNVkJhYzwmIPIqZPYy1vs7pI7N5IY";
+var bot = new TelegramBot(token, {polling:true});
+var chatId;
+bot.onText(/eae/,function(msg,match){
+	chatId = msg.chat.id;
+  bot.sendMessage(chatId,'Diz ae');
+})
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on("chat message", msg => {
+    console.log(msg);
+    io.emit("chat message", msg);
+    bot.sendMessage(chatId,msg);
+  });
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-server.listen(port, () => {
-    console.log('listening on '+ port);
-});
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
